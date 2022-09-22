2b7ba87c9638b3ecd49c4897148a18fb14e13694
Chiam <chiamyeehang@gmail.com>
2022-09-22 23:06:24 +0800

    update query 1

--- CYH/query1.sql
+++ CYH/query1.sql
@@ -1,4 +1,4 @@
-select * from (select T.id Treatment_ID,T.name Treatment_Name,T.price,count(TI.invoiceId)Total_Quantity,sum(T.price * count(TI.invoiceId))Total_Profit
+select * from (select T.id Treatment_ID,T.name Treatment_Name,T.price,count(TI.invoiceId)Total_Quantity,(T.price * count(TI.invoiceId))Total_Profit
 from Treatments T, TreatmentInvoice TI
 where T.id = TI.treatmentId
 group by T.id,T.name,T.price
