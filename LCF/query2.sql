657664e2a840abcd0b7c057561b48d717563d3c8
Chun Fai <ashchunfai@gmail.com>
2022-09-22 23:13:33 +0800

    modified to become views

--- LCF/query2.sql
+++ LCF/query2.sql
@@ -1,7 +1,10 @@
 -- Retrieve all medications' stock quantities and its suppliers relevant details in an ascending order
 -- Monitor the medications' stock from lowest quantity to highest quantity
 -- Respective suppliers' details are displayed to provide convenience when needed to purchase more stock
+CREATE OR REPLACE VIEW MEDICATIONS_STOCK AS
 SELECT M.supplierId ID, M.name, S.name SUPPLIER_NAME, S.email SUPPLIER_EMAIL, M.stockQuantity STOCK_QUANTITY
 FROM Medications M, Suppliers S
 WHERE M.supplierId = S.id
 ORDER BY M.stockQuantity;
+
+SELECT * FROM MEDICATIONS_STOCK;
