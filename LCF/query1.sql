d575713cdc0d1c864ff662bc62aaefc97cece7e7
Chun Fai <42035327+chuunfaii@users.noreply.github.com>
2022-09-21 21:52:24 -0700

    Update query1.sql

--- LCF/query1.sql
+++ LCF/query1.sql
@@ -1,6 +1,5 @@
--- Retrieve the total customers and total sales from an area in Penang island in a descending order
+-- Retrieve the total customers and total sales from all areas in Penang island in a descending order
 -- Determine which area has the highest demand to possibly open a new branch in that area(s)
-
 SELECT A.area, COUNT(C.id) TOTAL_CUSTOMERS, TO_CHAR(SUM(D.totalPrice), '99,999.99') TOTAL_SALES
 FROM Customers A, Pets B, Appointments C, Invoices D
 WHERE B.ownerIc = A.ic AND C.petId = B.id AND D.appointmentId = C.id
