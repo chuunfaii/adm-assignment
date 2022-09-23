e22d190442e70b8eb84cb058888f2f0f04ced311
fallenhamster <khaigenequah100@gmail.com>
2022-09-24 03:35:59 +0800

    Testing trigger 1

--- QKG/trigger_1.sql
+++ QKG/trigger_1.sql
@@ -1,31 +1,31 @@
 CREATE OR REPLACE TRIGGER trg_staff_commission
-BEFORE UPDATE ON Staffs
+BEFORE UPDATE OF commission ON Staffs
 FOR EACH ROW 
 
 DECLARE 
-    id Staffs.id%TYPE;
-    v_commission Staffs.commission%TYPE;
+    v_id Staffs.id%TYPE;
     commission1 Staffs.commission%TYPE;
     commission2 Staffs.commission%TYPE;
     appointmentCount NUMBER;
 BEGIN
-    SELECT COUNT(*) INTO appointmentCount FROM Appointments WHERE Appointments.staffId = Staffs.id;
+    SELECT id INTO v_id FROM Staffs;
+    SELECT COUNT(id) INTO appointmentCount FROM Appointments WHERE Appointments.staffId = v_id;
     
 
-    commission1 := 200;
-    commission2 := 400;
+    --commission1 := 200;
+    --commission2 := 400;
 
     IF (appointmentCount < 5) THEN
         DBMS_OUTPUT.PUT_LINE('No Additional Commission.');
-    --ELSE IF (appointmentCount > = 5) THEN
-        --DBMS_OUTPUT.PUT_LINE('Get RM200 Of Commission.');
-        --UPDATE Staffs SET commission = commission1
-        --v_commission := v_commission + commission1;
-        --UPDATE Staffs SET commission = v_commission;
-    --ELSE IF (appointmentCount >= 10) THEN
-        --DBMS_OUTPUT.PUT_LINE('Get RM400 Of Commission.');
-        --v_commission := v_commission + commission2;
-        --UPDATE Staffs SET commission = v_commission;
     END IF;
+
+    IF (appointmentCount >= 5) THEN
+        DBMS_OUTPUT.PUT_LINE('Get RM200 Of Commission.');
+    END IF;
+
+    IF (appointmentCount >= 10) THEN
+        DBMS_OUTPUT.PUT_LINE('Get RM400 Of Commission.');   
+    END IF;
+
 END;
 /
