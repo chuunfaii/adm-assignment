ac9204fc5b0a73de8bb8532ff2a03418b8931a17
fallenhamster <khaigenequah100@gmail.com>
2022-09-25 16:21:57 +0800

    Modified query_1

--- QKG/query_1.sql
+++ QKG/query_1.sql
@@ -2,10 +2,10 @@
 --Retrieve total number of appointment handled by all staff in descending order
 --Help to assist management to make decision about promotion of the staff or additional commission for the staff
 CREATE OR REPLACE VIEW STAFFS_WORKLOAD AS
-SELECT S.id Staff_ID,S.name Staff_Name,COUNT(A.id) TOTAL_COUNTS
+SELECT S.id Staff_ID,S.name Staff_Name,COUNT(A.id) APPT_COUNTS
 FROM Staffs S left join Appointments A
 ON S.id = A.staffId
 GROUP BY S.id,S.name
-ORDER BY TOTAL_COUNTS DESC;
+ORDER BY APPT_COUNTS DESC;
 SELECT * FROM STAFFS_WORKLOAD;
 
