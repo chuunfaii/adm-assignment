ca62ab85b2b4f34b8ebe4c555d3bb2ac3086bde4
Chiam <chiamyeehang@gmail.com>
2022-09-25 15:47:48 +0800

    modify crt and done trigger 2

--- CYH/trigger2.sql
+++ CYH/trigger2.sql
@@ -1,21 +1,45 @@
-CREATE OR REPLACE TRIGGER staffDetailLog
-AFTER INSERT OR UPDATE ON Staffs
+DROP SEQUENCE StaffDetailLogID_Seq;
+
+CREATE SEQUENCE StaffDetailLogID_Seq
+MINVALUE 1
+START WITH 1
+INCREMENT BY 1
+NOCACHE;
+
+CREATE OR REPLACE TRIGGER staff_detail_log
+after UPDATE OF email,phoneNo,role ON Staffs
 FOR EACH ROW
 
 DECLARE
-    v_staffId staffs.id%TYPE;
-    v_email staffs.email%TYPE;
-    v_phoneNo staffs.phoneNo%TYPE;
-    v_role staffs.role%TYPE;
-    v_action varchar(10);
-    v_updatedData varchar(100);
-    v_chgDesc varchar(200);
-
+    EX_INVALID_EMAIL EXCEPTION;
+    EX_INVALID_PHONE_NO EXCEPTION;
+    EX_INVALID_ROLE EXCEPTION;
 BEGIN
     CASE
-    WHEN inserting THEN
-        v_action := 'Inserted';
-        v_staffId := new.id;
-    
-    WHEN updating THEN
+        WHEN (:new.email IS NULL) THEN
+            RAISE EX_INVALID_EMAIL;
+        WHEN (:new.phoneNo IS NULL) THEN
+            RAISE EX_INVALID_PHONE_NO;
+        WHEN (:new.role IS NULL) THEN
+            RAISE EX_INVALID_ROLE;
+        ELSE
+            insert into StaffDetailLog
+            values(StaffDetailLogID_Seq.nextval,:OLD.id, :NEW.email, :OLD.email, :NEW.phoneNo, :OLD.phoneNo,
+            :NEW.role, :OLD.role, SYSDATE);
+
+    END CASE;
+
+    EXCEPTION
+        WHEN EX_INVALID_EMAIL THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Invalid email.');
+        WHEN EX_INVALID_PHONE_NO THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Invalid phone number.');
+        WHEN EX_INVALID_ROLE THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Invalid role.');
+
+
+
+end;
+/
+
         
